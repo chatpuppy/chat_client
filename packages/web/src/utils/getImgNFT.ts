39a7dadff2dc0e02dacac311021afb0d9b3e1e9f
edward <luuleminhthanh120493@gmail.com>
2022-04-27 17:52:06 +0700

    fix link uri of moralis

--- packages/web/src/utils/getImgNFT.ts
+++ packages/web/src/utils/getImgNFT.ts
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
 export const getImgNFT = async(url: string) => {
-    return await axios.get(url)
+    
+    let urlRequest = ''
+    if(url && url.includes('https://ipfs.moralis.io:2053')) {
+      urlRequest = url.replace('https://ipfs.moralis.io:2053', 'https://gateway.moralisipfs.com')
+    }
+    return await axios.get(urlRequest)
     .then(function (response) {
       // handle success
       return response.data.thumbnail || response.data.image || response.data.image_url || response.data.background_image
